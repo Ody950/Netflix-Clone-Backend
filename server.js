9313da772dcc01b343738b16105fb76a78f8e6b0
Ody950 <odai.al.obeidat950@gmail.com>
2023-05-17 06:08:30 +0000

    push3

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ require('dotenv').config();
 const pg = require('pg')
 server.use(express.json());
 server.use(cors());
-// process.env.PORT || 
-let PORT = 3017;
+let PORT =  3025||process.env.PORT ;
 const axios = require('axios');
 const apKey = process.env.APIkey;
 const client = new pg.Client(process.env.DATABASE_URL)
@@ -18,7 +17,12 @@ server.get('/trending', trendingMov);
 
 server.post('/addToFav', addToFav);
 
-server.get('/favMovies', getfavMovies)
+server.get('/favMovies', getfavMovies);
+
+server.put('/update/:id', putupdate);
+
+server.delete('/DELETE/:id', deletMovie);
+
 
 function trendingMov(req, res) {
 
@@ -73,7 +77,50 @@ function getfavMovies(req,res){
   }).catch((error)=>{
       errorHandler(error,req,res)
   })
+};
+
+
+
+function putupdate(req,res){
+  const id = req.params.id;
+  const upfavMovie = req.body;
+  const sql = `UPDATE favMovies
+  SET note = $1 WHERE id = ${id} RETURNING *;`;
+    const values = [upfavMovie.note];
+    client.query(sql,values)
+    .then(data=>{
+      const sql = `SELECT * FROM favMovies;`;
+            client.query(sql)
+                .then(allData => {
+                    res.send(allData.rows)
+                })
+                .catch((error) => {
+                    errorHandler(error, req, res)
+                })
+  })
+  .catch((error)=>{
+    console.log('sorry you have something error',error)
+    res.status(500).send(error);})
+
 }
+
+
+
+function deletMovie(req, res){
+
+  const id = req.params.id;
+  const sql = `DELETE FROM favMovies WHERE id=${id} RETURNING *;`;
+  
+  client.query(sql)
+  .then((delM)=>{
+res.send("The data has been deleted successfully")
+})
+  .catch((error)=>{
+    errorHandler(error,req,res)
+  })
+  }
+
+
 
 
 
