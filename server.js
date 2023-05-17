138380b6a2fff0b04f86814c212c88e0cd7777e0
Ody950 <odai.al.obeidat950@gmail.com>
2023-05-17 08:30:47 +0000

    push9

--- server.js
+++ server.js
@@ -38,9 +38,10 @@ function trendingMov(req, res) {
       })
       res.send(mapResults)
     })
-    .catch((error)=>{
-      console.log('sorry you have something error',error)
-      res.status(500).send(error);})
+    .catch((error) => {
+      console.log('sorry you have something error', error)
+      res.status(500).send(error);
+    })
 };
 
 
@@ -49,76 +50,82 @@ function trendingMov(req, res) {
 function addToFav(req, res) {
 
 
-    console.log('We got fav movie from the FE')
-    console.log(req.body)
-    const favMovie = req.body
-    const sql = `INSERT INTO favMovies (title, overview, poster_path, note)
+  console.log('We got fav movie from the FE')
+  console.log(req.body)
+  const favMovie = req.body
+  const sql = `INSERT INTO favMovies (title, overview, poster_path, note)
     VALUES ($1,$2, $3, $4);`
-    const values = [favMovie.title, favMovie.overview,favMovie.poster_path,
-      favMovie.note];
-    client.query(sql,values)
-    .then(data=>{
-        res.send("The data has been added successfully" + values);
+  const values = [favMovie.title, favMovie.overview, favMovie.poster_path,
+  favMovie.note];
+  client.query(sql, values)
+    .then(data => {
+      res.send("The data has been added successfully" + values);
     })
-    .catch((error)=>{
-      console.log('sorry you have something error',error)
-      res.status(500).send(error);})
+    .catch((error) => {
+      console.log('sorry you have something error', error)
+      res.status(500).send(error);
+    })
 
 }
 
 
 
-function getfavMovies(req,res){
+function getfavMovies(req, res) {
   const sql = `SELECT * FROM favMovies;`;
   client.query(sql)
-  .then(data=>{
+    .then(data => {
       res.send(data.rows);
       console.log('data from DB', data.rows)
-  }).catch((error)=>{
-      errorHandler(error,req,res)
-  })
+    })
+    .catch((error) => {
+      console.log('sorry you have something error', error)
+      res.status(500).send(error);
+    })
 };
 
 
 
-function putupdate(req,res){
+function putupdate(req, res) {
   const id = req.params.id;
   const upfavMovie = req.body;
   const sql = `UPDATE favMovies
   SET note = $1 WHERE id = ${id} RETURNING *;`;
-    const values = [upfavMovie.note];
-    client.query(sql,values)
-    .then(data=>{
+  const values = [upfavMovie.note];
+  client.query(sql, values)
+    .then(data => {
       const sql = `SELECT * FROM favMovies;`;
-            client.query(sql)
-                .then(allData => {
-                    res.send(allData.rows)
-                })
-                .catch((error) => {
-                    errorHandler(error, req, res)
-                })
-  })
-  .catch((error)=>{
-    console.log('sorry you have something error',error)
-    res.status(500).send(error);})
+      client.query(sql)
+        .then(allData => {
+          res.send(allData.rows)
+        })
+        .catch((error) => {
+          console.log('sorry you have something error', error)
+          res.status(500).send(error);
+        })
+    })
+    .catch((error) => {
+      console.log('sorry you have something error', error)
+      res.status(500).send(error);
+    })
 
 }
 
 
 
-function deletMovie(req, res){
+function deletMovie(req, res) {
 
   const id = req.params.id;
   const sql = `DELETE FROM favMovies WHERE id=${id} RETURNING *;`;
-  
+
   client.query(sql)
-  .then((delM)=>{
-res.send("The data has been deleted successfully")
-})
-  .catch((error)=>{
-    errorHandler(error,req,res)
-  })
-  }
+    .then((delM) => {
+      res.send("The data has been deleted successfully")
+    })
+    .catch((error) => {
+      console.log('sorry you have something error', error)
+      res.status(500).send(error);
+    })
+}
 
 
 
@@ -134,7 +141,7 @@ server.get('*', (req, res) => {
 
 
 
-function MoviesDa(id, title, release_date, poster_path, overview, ) {
+function MoviesDa(id, title, release_date, poster_path, overview,) {
   this.id = id;
   this.title = title;
   this.release_date = release_date;
@@ -145,10 +152,10 @@ function MoviesDa(id, title, release_date, poster_path, overview, ) {
 
 
 client.connect()
-.then(()=>{
+  .then(() => {
 
-  server.listen(PORT, () => {
-    console.log(`Listening on ${PORT}: I'm ready`)
+    server.listen(PORT, () => {
+      console.log(`Listening on ${PORT}: I'm ready`)
+    })
+
   })
-
-})
